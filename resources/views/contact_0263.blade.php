92995f5aa0e0962ef05b3a2b75e88d3e590e1e6b
Mafud <mafudnurkarim@gmail.com>
2021-05-22 17:20:56 +0700

    first commit

---
+++ resources/views/contact_0263.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Template Mo">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+
+    <title>Tugas Praktikum 1</title>
+
+    <!-- Additional CSS Files -->
+      <link rel="stylesheet" href="{{URL::asset('css/bootstrap.min.css')}}">
+      <link rel="stylesheet" href="{{URL::asset('css/font-awesome.css')}}">
+      <link rel="stylesheet" href="{{URL::asset('css/templatemo-art-factory.css')}}">
+      <link rel="stylesheet" href="{{URL::asset('css/owl-carousel.css')}}">
+
+    </head>
+    
+    <body>
+    
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+    
+    
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="#" class="logo">Mafud's</a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="#welcome" class="active">Home</a></li>
+                            <li class="scroll-to-section"><a href="#about">Article</a></li>
+                            <li class="scroll-to-section"><a href="#contact-us">Contact Us</a></li>
+                        </ul>
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
+<div class="welcome-area" id="welcome">
+
+<!-- ***** Header Text Start ***** -->
+<div class="header-text">
+    <div class="container">
+        <div class="row">
+            <div class="left-text col-lg-6 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                <h1>Tugas Praktikum <strong>P.T.W</strong></h1>
+                <p>ini adalah tugas praktikum pengembangan teknologi web</p>
+                <a href="#about" class="main-button-slider">Contact me</a>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                <!-- <img src="assets/images/slider-icon.png" class="rounded img-fluid d-block mx-auto" alt="First Vector Graphic"> -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ***** Header Text End ***** -->
+</div>
+
+
+<!-- Javascript -->
+
+<script src="{{URL::asset('js/jquery-2.1.0.min.js')}}"></script>
+<script src="{{URL::asset('js/popper.js')}}"></script>
+<script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
+<script src="{{URL::asset('js/owl-carousel.js')}}"></script>
+<script src="{{URL::asset('js/scrollreveal.min.js')}}"></script>
+<script src="{{URL::asset('js/waypoints.min.js')}}"></script>
+<script src="{{URL::asset('js/jquery.counterup.min.js')}}"></script>
+<script src="{{URL::asset('js/imgfix.min.js')}}"></script>
+<script src="{{URL::asset('js/custom.js')}}"></script>
+<!-- end Javascript -->
+</body>
+</html>
